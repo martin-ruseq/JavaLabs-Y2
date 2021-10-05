4cdd8381262637932ff6c275439daf4f5a8fba58
Martin <72360462+martin-ruseq@users.noreply.github.com>
2021-10-05 21:24:45 +0100

    Added thermB

--- all-labs/src/it/itcarlow/lab1/ThermTest.java
+++ all-labs/src/it/itcarlow/lab1/ThermTest.java
@@ -1,20 +1,28 @@
 package it.itcarlow.lab1;
 
-// Student Name 	: Oisin Cawley
-// Student Id Number: 
-// Date 			: Nov-2015
-// Purpose 			: My first class implementation
+// Student Name 	: Marcin Rusiecki
+// Student Id Number: C00263263
+// Date 			: 05/10/2021
+// Purpose 			: 
 
 public class ThermTest
 { // begin class ThermTest
+	
 	public static void main(String args[]) 
 	{ // being main method
-
+		
+		double tempB;
+		
 		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
+		Thermometer thermB = new Thermometer(10.0);		//Create second instance using second constructor
 
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		
 		thermA.setCelsius(20.0);
 		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		
+		tempB = thermB.getCelsius();	//Store the value of Thermometer B in variable
+		System.out.println("Temp. of Thermometer B is " + tempB);
 		
 	} // end main
 } // end class ThermTest
