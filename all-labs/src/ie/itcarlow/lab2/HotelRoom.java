6e4d6440fcccffe720acdcdeb7021a22b6efc7b3
Martin <72360462+martin-ruseq@users.noreply.github.com>
2021-10-08 12:23:14 +0100

    Initial Commit

---
+++ all-labs/src/ie/itcarlow/lab2/HotelRoom.java
@@ -0,0 +1,45 @@
+package ie.itcarlow.lab2;
+
+//Student Name: 	Marcin Rusiecki
+//Student ID: 		C00263263
+//Date: 			05/10/2021
+//Purpose: 
+
+public class HotelRoom 
+{
+	private int	roomNumber;
+	private String roomType;
+	
+	public HotelRoom()								// constructor method #1
+	{
+		setNumber(0);	
+		setType("");
+	}
+	
+	public HotelRoom(int num, String type)			// constructor method #2
+	{
+		setNumber(num);
+		setType(type);		
+	}
+	
+	public void setNumber(int num)
+	{
+		roomNumber = num;
+		
+	}
+	
+	public void setType (String type)
+	{
+		roomType = type;
+	}
+	
+	public int getNumber()
+	{
+		return roomNumber;
+	}
+	
+	public String gettyp()
+	{
+		return roomType;
+	}
+}
