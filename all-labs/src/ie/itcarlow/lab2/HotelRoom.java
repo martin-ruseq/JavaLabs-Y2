ed55d60126420e42e3e7f249b8e329c24c6fb625
Martin <72360462+martin-ruseq@users.noreply.github.com>
2021-10-12 12:12:28 +0100

    Added methods for room status and rate

--- all-labs/src/ie/itcarlow/lab2/HotelRoom.java
+++ all-labs/src/ie/itcarlow/lab2/HotelRoom.java
@@ -9,19 +9,28 @@ public class HotelRoom
 {
 	private int	roomNumber;
 	private String roomType;
+	private int roomStatus;
+	private double nightRate;
 	
-	public HotelRoom()								// constructor method #1
+	//empty constructor
+	public HotelRoom()								
 	{
 		setNumber(0);	
 		setType("");
+		setStatus(0);
+		setRate(0.0);
 	}
 	
-	public HotelRoom(int num, String type)			// constructor method #2
+	//Overloaded constructor
+	public HotelRoom(int num, String type, int stat, double rate)			
 	{
 		setNumber(num);
-		setType(type);		
+		setType(type);
+		setStatus(stat);
+		setRate(rate);
 	}
 	
+	//Setters
 	public void setNumber(int num)
 	{
 		roomNumber = num;
@@ -33,6 +42,17 @@ public class HotelRoom
 		roomType = type;
 	}
 	
+	public void setStatus (int stat)
+	{
+		roomStatus = stat;
+	}
+	
+	public void setRate (double rate)
+	{
+		nightRate = rate;
+	}
+	
+	//Getters
 	public int getNumber()
 	{
 		return roomNumber;
@@ -41,5 +61,15 @@ public class HotelRoom
 	public String getTyp()
 	{
 		return roomType;
+	}
+	
+	public int getStatus()
+	{
+		return roomStatus;
+	}
+	
+	public double getRate()
+	{
+		return nightRate;
 	}
 }
