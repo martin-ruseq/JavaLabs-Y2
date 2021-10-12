4c7a5ae39b71f0ee62734099c8f8877a18e04dca
Martin <72360462+martin-ruseq@users.noreply.github.com>
2021-10-12 12:11:12 +0100

    Initial commit

---
+++ all-labs/src/ie/itcarlow/lab2/Lab2Q2.java
@@ -0,0 +1,48 @@
+
+package ie.itcarlow.lab2;
+
+//Student Name: 	Marcin Rusiecki
+//Student ID: 		C00263263
+//Date: 			12/10/2021
+//Purpose: 			Set Room Status and Room Nihtly Rate, 
+//					and print the information about the rooms using appriopiate get method
+
+public class Lab2Q2 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		HotelRoom roomA;
+		HotelRoom roomB;
+		
+		roomA = new HotelRoom();
+		roomB = new HotelRoom();
+		
+		roomA.setNumber(200);
+		roomA.setType("Single");
+		roomA.setStatus(1);
+		roomA.setRate(100);
+		
+		roomB.setNumber(201);
+		roomB.setType("Double");
+		roomB.setStatus(0);
+		roomB.setRate(80);
+		
+		System.out.println("## Room A ##");
+		System.out.println("Room number: " + roomA.getNumber());
+		System.out.println("Room Type:" + roomA.getTyp());
+		System.out.println("Room Status (0 = vaant ; 1 = occupied): " + roomA.getStatus());
+		System.out.println("Room Nigtly Rate: " + roomA.getRate());
+		System.out.println("");
+
+		System.out.println("## Room B ##");
+		System.out.println("Room number: " + roomB.getNumber());
+		System.out.println("Room Type: " + roomB.getTyp());
+		System.out.println("Room Status (0 = vaant ; 1 = occupied): " + roomB.getStatus());
+		System.out.println("Room Nigtly Rate: " + roomB.getRate());
+		System.out.println("");
+		
+	}
+
+}
