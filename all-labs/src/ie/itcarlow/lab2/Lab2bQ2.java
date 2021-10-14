a92b643a3ce9a7a447338f3b8cab407ccffbbe9c
Martin <72360462+martin-ruseq@users.noreply.github.com>
2021-10-14 18:32:49 +0100

    Initial Commit

---
+++ all-labs/src/ie/itcarlow/lab2/Lab2bQ2.java
@@ -0,0 +1,24 @@
+package ie.itcarlow.lab2;
+
+//Student Name: 	Marcin Rusiecki
+//Student ID: 		C00263263
+//Date: 			14/10/2021
+//Purpose:
+
+public class Lab2bQ2 
+{
+
+	public static void main(String[] args) 
+	{
+		Rectangle fig1;
+		
+		fig1 = new Rectangle();
+		
+		fig1.setLength(25);
+		fig1.setWidth(10);
+		
+		System.out.println(fig1.toString());
+
+	}
+
+}
