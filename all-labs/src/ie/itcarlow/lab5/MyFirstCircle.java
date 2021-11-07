4da5eddd84a1a45c03e25d635978786e0c262207
Martin <72360462+martin-ruseq@users.noreply.github.com>
2021-11-07 15:44:06 +0000

    Initiall Commit

---
+++ all-labs/src/ie/itcarlow/lab5/MyFirstCircle.java
@@ -0,0 +1,20 @@
+package ie.itcarlow.lab5;
+
+//Student Name: 	Marcin Rusiecki
+//Student ID: 		C00263263
+//Date: 			03/11/2021
+
+public class MyFirstCircle
+{
+
+	public static void main(String[] args)
+	{
+		/* Point myPoint = new Point(10, 20);
+		Circle myCircle = new Circle(15, 30, 5);
+		
+		System.out.println("Point details : " + myPoint);
+		System.out.println("Circle details: " + myCircle); */
+
+	}
+
+}
