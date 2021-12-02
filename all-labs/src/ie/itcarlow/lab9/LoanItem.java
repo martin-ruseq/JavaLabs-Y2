f8c131e153bb532d1ed8dad937ef3483e6f12e35
Martin <72360462+martin-ruseq@users.noreply.github.com>
2021-12-02 21:57:23 +0000

    Q1) Interface

---
+++ all-labs/src/ie/itcarlow/lab9/LoanItem.java
@@ -0,0 +1,11 @@
+package ie.itcarlow.lab9;
+
+// Student Name:	Marcin Rusiecki
+// Student ID:		C00263263
+// Date:			30/11/2021
+
+public interface LoanItem
+{
+	//method
+	double calculatePrice();
+}
