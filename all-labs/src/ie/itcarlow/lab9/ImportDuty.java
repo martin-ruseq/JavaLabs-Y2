96267c4f5f2923f1629862459a12f3557a7681a3
Marcin Rusiecki <C00263263@itcarlow.ie>
2021-12-03 14:11:27 +0000

    Q2) Interface
    Signed-off-by: Marcin Rusiecki <C00263263@itcarlow.ie>

---
+++ all-labs/src/ie/itcarlow/lab9/ImportDuty.java
@@ -0,0 +1,15 @@
+package ie.itcarlow.lab9;
+
+// Student Name:	Marcin Rusiecki
+// Student ID:		C00263263
+// Date:			02/12/2021
+
+public interface ImportDuty
+{
+	//constants
+	final double CARTAXRATE = 0.10;
+	final double HGVTAXRATE = 0.15;
+	
+	//method
+	double calculateDuty();
+}
