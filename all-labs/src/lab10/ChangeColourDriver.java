d63f3f675eac5bbc3ef1214b265afaec616281c9
Marcin Rusiecki <C00263263@itcarlow.ie>
2022-01-11 11:26:00 +0000

    Initial commit (lab10)
    Signed-off-by: Marcin Rusiecki <C00263263@itcarlow.ie>

---
+++ all-labs/src/lab10/ChangeColourDriver.java
@@ -0,0 +1,20 @@
+package lab10;
+
+//Student Name:		Marcin Rusiecki
+//Student ID:		C00263263
+//Date:				11/01/2022
+
+import javax.swing.JFrame;
+
+public class ChangeColourDriver {
+
+	public static void main(String[] args) {
+	
+		ChangeColour changeColour = new ChangeColour("Button");
+		changeColour.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		changeColour.setSize(500, 300);
+		changeColour.setVisible(true);
+
+	}//end main
+
+}//end class
